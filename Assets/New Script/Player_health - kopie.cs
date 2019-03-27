4c3e0515435e957b2e9e6bdc43202f305fcf3fef
saoud <saoud_2000@live.nl>
2019-03-27 13:04:58 +0100

    versie 1.2

---
+++ Assets/New Script/Player_health - kopie.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class Player_health : MonoBehaviour
+{
+
+	public int health;
+	
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (gameObject.transform.position.y < -7)
+		{
+			Die();
+		}
+
+	}
+		
+
+    void Die()
+	{
+		SceneManager.LoadScene ("level1");
+	}
+}
